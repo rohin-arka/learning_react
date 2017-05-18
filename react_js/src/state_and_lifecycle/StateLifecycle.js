2babe113ac15abcac30ac66d303e51e5e5429367
rohin-arka <surya_siwakoti@outlook.com>
2017-05-18 09:48:27 +0545

    completed lesson data flows down

--- react_js/src/state_and_lifecycle/StateLifecycle.js
+++ react_js/src/state_and_lifecycle/StateLifecycle.js
@@ -95,3 +95,35 @@ class Clock extends React.Component{
 }
 
 ReactDOM.render(<Clock />, document.getElementById('root'));
+// Recap
+// When <Clock /> is passed to ReactDOM.render(), React calls the constructor of the Clock
+// component. Since Clock needs to display the current time, it initializes this.state with an object
+// including the current time. We will later update this state.
+//
+// React then calls the Clock component's render() method.
+// This is how React learns what should be displayed on the screen.
+// React then updates the DOM to match the Clock's render output.
+//
+// When the Clock output is inserted in the DOM, React calls the componentDidMount()
+// lifecycle hook. Inside it, the Clock component asks the browser to set up a timer to call
+// tick() once a second.
+//
+// Every second the browser calls the tick() method. Inside it, the Clock component
+// schedules a UI update by calling setState() with an object containing the current time.
+// Thanks to the setState() call, React knows the state has changed, and calls render() method again
+// to learn what should be on the screen. This time, this.state.date in the render() method will be
+// different, and so the render output will include the updated time. React updates the DOM accordingly.
+//
+// If the Clock component is ever removed from the DOM, React calls the componentWillUnmount()
+// lifecycle hook so the timer is stopped.
+
+// Use state correctly
+this.state.stateName = 'hola'; // this is bad
+this.setState({ stateName: 'voila' }); // React provudes setState methodwhich can change state.
+// NOTE: this.state is only allow initialially in constructor
+// The data flows down
+// Neither parent nor children components can know if a certain component is stateful or stateless
+// This is why state is often called local or  encapsulated.
+// Component can pass its state down as props to its child components
+// <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+// or as a props <FormattedDate date={this.state.date} />
